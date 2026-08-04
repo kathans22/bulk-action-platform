31c782b2022ab7de2903fb0ea943cf51c5d54262
kathans22 <kathanshah2215@gmail.com>
2026-08-04 23:10:01 +0530

    feat: add entity id fetching and batch chunking

--- src/services/bulkActions.service.js
+++ src/services/bulkActions.service.js
@@ -1,5 +1,6 @@
 const { getEntityConfig } = require('../entities');
 const { getActionHandler } = require('../actions');
+const { getContactIdsByAccount } = require('../queries/contacts.queries');
 
 function badRequest(message) {
   const error = new Error(message);
@@ -26,4 +27,24 @@ function validateRequest({ accountId, entityType, actionType, configuration }) {
   }
 }
 
-module.exports = { validateRequest };
+function chunkArray(array, size) {
+  const chunks = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+
+  return chunks;
+}
+
+// The ids the action will act on. Contacts are the only entity today, so a
+// second entity means one more line here.
+function fetchEntityIds(entityType, accountId) {
+  if (entityType === 'contact') {
+    return getContactIdsByAccount(accountId);
+  }
+
+  throw badRequest(`No id lookup for entity type "${entityType}"`);
+}
+
+module.exports = { validateRequest, chunkArray, fetchEntityIds };
