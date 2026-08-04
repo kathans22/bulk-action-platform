90d86e01d0f8304b220de4944347702dc12fd921
kathans22 <kathanshah2215@gmail.com>
2026-08-04 22:34:38 +0530

    feat: add postgres connection pool and query helper

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+function query(text, params) {
+  return pool.query(text, params);
+}
+
+module.exports = { pool, query };
